d31263a63f069f707278dc53b8aa416968b456a7
Nitin Chaudhary <nitin.io@outlook.com>
2022-10-17 21:51:23 +0530

    add useful-software-script

---
+++ essential-softwares-for-linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -e "\n\n....Installing GIT (Global Information Tracker)....\n"
+sudo apt -y install git
+echo -e "\n\n....Installing VLC media player....\n"
+sudo apt -y install vlc
+echo -e "\n\n....Installing OBS Studio....\n"
+sudo apt -y install obs-studio
+echo -e "\n\n....Installing Aircrack-NG....\n"
+sudo apt -y install aircrack-ng
+echo -e "\n\n....Everything Is Done From My Side....\n"
+
+
