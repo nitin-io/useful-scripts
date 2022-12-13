9f2276757c8c48d5c22a11ad6b6d99d0e8a11461
Nitin Chaudhary <nitin.io@outlook.com>
2022-12-13 15:21:16 +0530

    updated

--- essential-softwares-for-linux.sh
+++ essential-softwares-for-linux.sh
@@ -17,3 +17,9 @@ sudo apt -y install openjdk-17-jdk
 sudo snap install code --classic
 
 sudo snap install spotify
+
+
+# Downloading and Installing google-chrome from official source.
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
